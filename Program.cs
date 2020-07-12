9414416cf36a5396f6eb1f948dd262c744c3575a
jcansdale-test <60072076+jcansdale-test@users.noreply.github.com>
2020-07-12 10:43:48 +0000

    Add dotnet project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace effective_spork_public
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
