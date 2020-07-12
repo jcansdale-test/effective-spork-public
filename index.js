7a8a42b01a0aa695c5c0c86235279958de62f72e
Jamie Cansdale <jcansdale@gmail.com>
2020-07-12 12:26:14 +0100

    Add Windows, Linux and MacOS support

--- index.js
+++ index.js
@@ -3,8 +3,15 @@
 const path = require('path');
 const { spawn } = require('child_process');
 
-let file = path.join(__dirname, "bin/effective-spork-public");
-const child = spawn(file, process.argv.slice(2));
+let platform = process.platform;
+let command = findCommand(platform);
+
+if (!command) {
+  console.error(`The ${platform} platform isn't currently supported.`);
+  process.exit(1)
+}
+
+const child = spawn(command, process.argv.slice(2));
 
 child.stdout.on('data', (data) => {
   console.log(`${data}`);
@@ -17,3 +24,14 @@ child.stderr.on('data', (data) => {
 child.on('close', (code) => {
   process.exit(code)
 });
+
+function findCommand(platform) {
+  switch(platform) {
+  case 'win32':
+    return path.join(__dirname, "bin", "win-x64", "effective-spork-public");
+  case 'linux':
+    return path.join(__dirname, "bin", "linux-x64", "effective-spork-public");
+  case 'darwin':
+    return path.join(__dirname, "bin", "osx-x64", "effective-spork-public");
+  }
+}
