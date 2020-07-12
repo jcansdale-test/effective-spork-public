edd1e3d685ebbf5d130b8f63be3dd581272abf21
Jamie Cansdale <jcansdale@gmail.com>
2020-07-13 00:59:55 +0100

    Take greeted as arg

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace effective_spork_public
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var who = args.Length > 0 ? args[0] : "World";
+            Console.WriteLine($"Hello, {who}!");
         }
     }
 }
